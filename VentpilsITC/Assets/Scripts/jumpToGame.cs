fad41be303c19fd197d8279fbf72dbc8e8778c73
Roberts Simanis <robsimanis@gmail.com>
2019-10-08 19:06:34 +0300

    Added the big bang scene

---
+++ VentpilsITC/Assets/Scripts/jumpToGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class jumpToGame : MonoBehaviour
+{
+    private float targetTime = 9.0f;
+
+
+    void Update()
+    {
+        targetTime -= Time.deltaTime;
+        if (targetTime <= 0.0f)
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
