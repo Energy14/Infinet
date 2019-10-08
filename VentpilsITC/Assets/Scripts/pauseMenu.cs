df49d6abf6749ed6155ffc1e1ca5132e4f5b2c2d
Roberts Simanis <robsimanis@gmail.com>
2019-10-08 20:18:41 +0300

    Added pause menu

---
+++ VentpilsITC/Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuObject;
+
+    public void onPause()
+    {
+        pauseMenuObject.SetActive(true);
+    }
+    public void onResumeGame()
+    {
+        pauseMenuObject.SetActive(false);
+    }
+    public void onExit()
+    {
+        Application.Quit();
+    }
+    public void onRestartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
